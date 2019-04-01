b1680b4e1862c597681c1a228281990a65e5719f
zhangye <zhangyet@gmail.com>
2019-04-01 21:42:26 +0800

    refactor
    1. use outputChannel to output rscript stdout.
    2. open output.html when finish compiling.
    3. format all ts file.

--- src/commands/build_book.ts
+++ src/commands/build_book.ts
@@ -1,61 +1,60 @@
-import * as child from 'child_process'; 
+import * as child from 'child_process';
 import * as vscode from 'vscode';
+
+import * as common from '../common';
 
 export async function buildBook(context: vscode.ExtensionContext) {
     // get workdir and store workdir
     // TODO: a more elegant way to do so
-    var defaultWorkDir = context.workspaceState.get<string>('defaultKamuiWorkDir');
-    if ( defaultWorkDir === undefined ) {
-        defaultWorkDir = '.';
+    var defaultWorkDir = context.workspaceState.get<string>(common.DefaultWorkDirKey);
+    if (defaultWorkDir === undefined) {
+        defaultWorkDir = __dirname; // default working dir is local dir
     }
     var path: string | undefined = await vscode.window.showInputBox({
         prompt: 'Enter bookdown project path.',
         value: defaultWorkDir,
         validateInput: (s: string): string | undefined => s && s.trim() ? undefined : "Bookdown path must not be empty",
     });
-    context.workspaceState.update('defaultKamuiWorkDir', path);
-    if ( path === undefined ) {
-        path = '.';
+    context.workspaceState.update(common.DefaultWorkDirKey, path);
+    if (path === undefined) {
+        path = __dirname;
     }
 
     // run the script and make process
-    var incre = 0;
     var args = [getScriptPath('build_book.R'), 'all', path];
     const s = child.spawn('Rscript', args);
+
+
+    var outputChannel = vscode.window.createOutputChannel(common.CompileOutputChannelName);
+    outputChannel.show(true);
     s.stdout.on('data', (data) => {
-        incre ++;
         console.log(`stdout: ${data}`);
-        if ( incre % 20 === 0 ) {
-            vscode.window.withProgress({
-                location: vscode.ProgressLocation.Window,
-			    title: "Bookdown is compiling...",
-			    cancellable: true
-            }, (progress, token) => {
-                token.onCancellationRequested(() => {
-                    s.send('close'); // terminate the compiling
-                    console.log('User canceled the long running operation');
-                });
-                progress.report({increment: incre, message: data.toString()});
-                var p = new Promise(resolve => {
-                    setTimeout(() => {
-                        resolve();
-                    }, 5000);
-                });
-                return p;
-            });
-        }
+        outputChannel.appendLine(data.toString());
     });
 
     s.on('close', (code) => {
         if (code === 0) {
-            vscode.window.showInformationMessage('compile finish');
+            vscode.window.showInformationMessage(common.BuildingSuccessfuleMsg);
+            if (path !== undefined) {
+                var openPath = cleanWorkPath(path) + common.OutputHtmlFile;
+                console.log('parsedPath: ' + openPath);
+                const open = require('open');
+                open(openPath);
+            }
         } else {
-            vscode.window.showErrorMessage('you fail me');
+            vscode.window.showErrorMessage(common.BuildingFailedMsg);
         }
     });
 }
 
-function getScriptPath(rfile:string):string {
+function getScriptPath(rfile: string): string {
     var rscriptDir = __dirname.replace('out/commands', 'R/');
     return rscriptDir + rfile;
+}
+
+function cleanWorkPath(path: string): string {
+    const homedir = require('os').homedir();
+    var cleanPath = path.replace('~', homedir);
+    const resolvedPath = require('path').resolve(cleanPath);
+    return resolvedPath;
 }
