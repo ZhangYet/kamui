5f64821614eea4afa821427c2abe1f3e50317217
zhangye <zhangyet@gmail.com>
2019-04-05 05:38:46 +0800

    update to 0.1.1, add error message output

--- src/common.ts
+++ src/common.ts
@@ -3,4 +3,5 @@ export const BuildingProgressTitle: string = 'bookdown is building';
 export const BuildingSuccessfuleMsg: string = 'bookdown building is completed successfully';
 export const BuildingFailedMsg: string = 'bookdown building failed';
 export const CompileOutputChannelName: string = 'compile output';
+export const CompileErrorChannelName: string = 'error';
 export const OutputHtmlFile: string = '/_book/output-of-presentation.html';
